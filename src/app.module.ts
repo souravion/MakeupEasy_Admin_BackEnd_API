42a866acf3e7c99feacdea7cf27fb7367c301ed9
sourav@ion <sourav.ion@gmail.com>
2023-09-05 00:22:02 +0530

    add category for mobile app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { config } from './config/config';
 import { CategoriesModule } from './admin/categories/categories.module';
+// CategoriesModule
 import { BannersModule } from './admin/banners/banners.module';
 import { FaqModule } from './admin/faq/faq.module';
 import { UserAuthModule } from './users/auth/auth.module';
 import { AdminAuthModule } from './admin/admin_auth/admin_auth.module';
 import { AdminUserModule } from './admin/admin_user/admin_user.module';
+import { CategoriesModule as AppCategoriesModule } from './users/categories/categories.module';
 
 @Module({
   imports: [
@@ -59,6 +61,7 @@ import { AdminUserModule } from './admin/admin_user/admin_user.module';
     BannersModule, 
     FaqModule,
     UserAuthModule,
+    AppCategoriesModule
   ],
     controllers: [AppController],
     providers: [AppService],
