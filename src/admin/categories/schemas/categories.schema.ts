b5cf7d66297e9b97f3a0d6169989afa62c553b6a
sourav@ion <sourav.ion@gmail.com>
2023-09-09 22:42:42 +0530

    add status in schema

--- src/admin/categories/schemas/categories.schema.ts
+++ src/admin/categories/schemas/categories.schema.ts
@@ -43,6 +43,9 @@ export class Categories {
   @Prop({ type: String, required: true})
   image: string
   
+  @Prop({ default: 'inactive', enum: ['active', 'inactive'] }) 
+  status: string;
+
   @Prop( { type: Date, default: Date.now })
   created_at:Date;
 
